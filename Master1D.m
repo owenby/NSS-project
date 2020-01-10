52587f5aec6f24b2b1965bc71c5d4ae988a1eca3
Owen <s1712037@ed.ac.uk>
2020-01-10 12:13:57 +0000

    Update to Master1D.m
    Added basic bar, stiffness, and frequency dependent loss. Need to add more boundary conditions for bar and stiffness cases I think, also wondering if there's a better way to implement all these effects. FDL sounds good! Bar and stiff sound pretty bad, but might be because I haven't found the right variables yet

--- Master1D.m
+++ Master1D.m
@@ -1,16 +1,24 @@
+
 clear all
 close all
 
-
 % Things to set 
-Boundary = 'free';       % Boundary condition (free, fixed, LfreeRfixed, RfreeLfixed)
-Starting = false;        % Whether the string starts with a starting position (true) or is flat (false)
-force = 'pluck';         % What kind of force applied to the string ('off' for no force)
-Effects = 'realistic';   % Currently just damping. 'loss' has just one type of loss, 'realistic' has two
+Boundary = 'fixed';    % Boundary condition (free, fixed, LfreeRfixed, RfreeLfixed)
+Starting = true;       % Whether the string starts with a starting position (true) or is flat (false)
+force = 'off';         % What kind of force applied to the string ('off' for no force)
+Effects = 'fdl';       % 'loss' has just one type of loss
+                       % 'realistic' has two.
+                       % 'bar' is a bar
+                       % 'stiff' is a soft bar
+                       % 'fdl' is frequency dependent loss
+                       
+Synthesize = 'play';         % Whether you see string ('plot'), hear string ('play'), or see the sound ('plotsound')
+
+
 
 
 % Variable setup
-Fs = 44100;      % Sample rate
+Fs = 44100;        % Sample rate
 k = 1/Fs;          % Time step
 T = 1;             % end time
 L = 1;             % Length of string
@@ -19,7 +27,13 @@ h = c*k;           % Grid spacing
 Ns = floor(T/k);   % Number of samples
 N = floor(L/h);    % N
 h = L/N;           % Redefine h so it matches with N
-
+% Stiffness variables
+E = 200;             % Youngs modulus of material
+p = 5000;             % Density
+I = 0.5;             % Bar moment of inertia (I = R/2 for cylinder)
+A = 1;             % Cross-sectional area
+kappa = sqrt((E*I)/(p*A*L^4));   % Stiffness parameter
+theta = 1;         % 'free parameter'
 
 
 % Starting conditions for string
@@ -27,14 +41,11 @@ u = zeros(Ns,N+1);
 if Starting 
     u(1,(round(N/2-5):round(N/2+5))) = hann(11);
     u(2,(round(N/2-5):round(N/2+5))) = hann(11);
-%    u(1,:) = hann(N+1);
-%    u(2,:) = hann(N+1);
+%   u(1,:) = hann(N+1);
+%   u(2,:) = hann(N+1);
     u(1,1) = 0;
     u(1,N+1)=0;
 end
-
-
-
 
 % Adding an external force
 a = zeros(N+1,1);
@@ -54,13 +65,21 @@ end
 
 
 
+
+
+
 % Initialising the matrices for system
 b0 = 1;                % First loss term (damping coefficient)
-b1 = 0.05;                % Second loss term
+b1 = 0.05;             % Second loss term (realistic coefficient)
 
 nOnes = ones(N+1, 1) ;
+
 Dxx = (1/h^2)*(-2*diag(nOnes, 0) + diag(nOnes(1:N), -1) + diag(nOnes(1:N), 1));
+Dxxxx = (1/h^4)*(diag(nOnes(1:N-1), -2)-4*diag(nOnes(1:N), -1) + 6*diag(nOnes, 0) - 4*diag(nOnes(1:N), 1) + diag(nOnes(1:N-1), 2));
+Mx = (1/2) * (diag(nOnes(1:N), -1) + diag(nOnes(1:N), 1));
 Dxx = sparse(Dxx);
+Dxxxx = sparse(Dxxxx);
+Mx = sparse(Mx);
 
 switch Effects
     case 'loss'
@@ -73,18 +92,29 @@ switch Effects
         A = (1 + b0*k/2) * eye(N+1,N+1) - b1*k/2*Dxx;
         B = 2*eye(N+1,N+1) + c^2*k^2*Dxx;
         C = A - 2*eye(N+1,N+1);
+    case 'bar'
+        A = theta * eye(N+1,N+1) + (1-theta) * Mx ;
+        B = -kappa^2 * k^2 * Dxxxx + 2*A;
+        C = -A;
     case 'stiff'
-        
+        A = theta * eye(N+1,N+1) + (1-theta) * Mx ;
+        B = k^2*c^2*Dxx - kappa^2*k^2*Dxxxx + 2*A;
+        C = -A;
+    case 'fdl'
+        A = (1+b0*k)*eye(N+1,N+1) - b1*k*Dxx;
+        B = -2*eye(N+1,N+1) - c^2*k^2*Dxx + kappa^2*k^2*Dxxxx;
+        C = (1+b0*k)*eye(N+1,N+1) + b1*k*Dxx;
+        A = -A; % It works :D 
 end
      
+
+
+
 
 % Making the matrices sparse martrices for optimality 
 A = sparse(A);
 B = sparse(B);
 C = sparse(C);
-
-
-
 
 % Switching the cases of the boundary out of 4 options
 switch Boundary
@@ -105,25 +135,51 @@ end
 
 
 
-out = zeros(1,Ns-1);  % To hear the sound
 
+% Switch to synthesize the sound
 
-% Loop to show the string over time
-for i=2:Ns-1
+switch Synthesize
+    case 'plot'
+        % Loop to show the string over time
+        for i=2:Ns-1
 
-    % Calculates next step in time
-    u(i+1,:) = A\(B*u(i,:)'+C*u(i-1,:)' + f(i)*a);
+            % Calculates next step in time
+            u(i+1,:) = A\(B*u(i,:)'+C*u(i-1,:)' + f(i)*a);
 
-    % Plots string in real time
-    plot(u(i,:));
-    axis([0,N,-1,1]);
-    drawnow
-    %out(i) = u(i,round(N/3));
+            % Plots string in real time
+            plot(u(i,:));
+            axis([0,N+1,-1,1]);
+            drawnow
 
+        end
+        
+    case 'play'
+        out = zeros(1,Ns-1);  % To hear the sound
+        % Loop to show the string over time
+        for i=2:Ns-1
+
+            % Calculates next step in time
+            u(i+1,:) = A\(B*u(i,:)'+C*u(i-1,:)' + f(i)*a);
+
+            % Vibration of one point on string
+            out(i) = u(i,round(N/3));
+
+        end
+        soundsc(out)    % To hear sound
+       
+        
+    case 'plotsound'
+        out = zeros(1,Ns-1);  % To hear the sound
+        % Loop to show the string over time
+        for i=2:Ns-1
+
+            % Calculates next step in time
+            u(i+1,:) = A\(B*u(i,:)'+C*u(i-1,:)' + f(i)*a);
+
+            % Vibration of one point on string
+            out(i) = u(i,round(N/3));
+
+        end
+        plot(out)       % To plot the sound 
 end
-
-% soundsc(out)    % To hear sound
-% plot(out)      % To plot the sound 
- 
- 
  
